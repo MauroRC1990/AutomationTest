06e457d129d18fabf618698c5b8400ecea017e44
MauroRC1990 <mrcolonna.pr@gmail.com>
2023-04-11 14:53:35 -0300

    Corrected a wait in LogoutTest class and verifying that gitignore works

--- src/test/java/com/automationtest/testng/LogoutTest.java
+++ src/test/java/com/automationtest/testng/LogoutTest.java
@@ -2,13 +2,16 @@ package com.automationtest.testng;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.Duration;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
@@ -31,6 +34,11 @@ public class LogoutTest {
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get(testValuesReader.getValue("url"));
+		
+		wait = new FluentWait<WebDriver>(driver)
+				.withTimeout(Duration.ofSeconds(60))
+				.pollingEvery(Duration.ofMillis(1000))
+				.ignoring(NoSuchElementException.class);
 
 		OrangeHRMPage orangeHRMPage = new OrangeHRMPage(driver, wait);
 		orangeHRMPage.waitForPresenceOfElement("//button[@type='submit']");
